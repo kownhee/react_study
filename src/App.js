3c8c99cd29016f60dc0c7b1e81683b3250e64363
kownhee <qkrrnjsgml7@naver.com>
2022-02-27 16:00:20 +0900

    add click event

--- src/App.js
+++ src/App.js
@@ -12,23 +12,57 @@ class App extends Component {
  {
    super(props);
    this.state = {
-    subject:{title:'PKH', sub:'web is good'},
+    mode : 'welcome',
+    selected_content_id : 0,
+    subject:{title:'WEB', sub:'web is good'},
+    welcome : {title : 'Welcome', desc : 'Hello, React!!'},
     contents:[
       {id:1 , title:'HTML', desc :'HTML is for information'},
-      {id:2 , title:'CSS', dsec :'Css if for dessign'},
-      {id:3 , title :'JavaScript', dsec:'JavaScript is for inters' }
+      {id:2 , title:'CSS', desc :'Css if for dessign'},
+      {id:3 , title :'JavaScript', desc:'JavaScript is for inters' }
     ]
    }
  }
 
   render(){
+    var _title, _desc = null;
+
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    }else if(this.state.mode === 'read'){
+        var i =0;
+        while(i < this.state.contents.length){
+          var data = this.state.contents[i];
+          if(data.id === this.state.selected_content_id){
+            _title = data.title;
+            _desc = data.desc;
+            break;
+          }
+          i=i+1;
+        }
+      
+    }
+
     return (
       <div className="App">
         <Subject
           title={this.state.subject.title}
-          sub={this.state.subject.sub}/>
-        <TOC data={this.state.contents}/>
-        <Content/>
+          sub={this.state.subject.sub}
+          onChangePage={function(){
+            this.setState({mode:'welcome'});
+          }.bind(this)}
+          />
+
+        <TOC onChangePage={function(id){
+          this.setState({
+            mode : 'read',
+            selected_content_id:Number(id)
+          });
+        }.bind(this)}
+        data={this.state.contents}/>
+
+        <Content title={_title} desc={_desc}/>
       </div>
     );
   }
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -8,7 +8,17 @@ class TOC extends Component
     var i = 0;
 
     while(i < data.length){
-      lists.push(<li><a href={"/content/"+data[i].id}>{data[i].title}</a></li>)
+      lists.push(
+        <li key={data[i].id}>
+         <a 
+          href={"/content/"+data[i].id}
+          onClick={function(id, e){
+            e.preventDefault();
+            this.props.onChangePage(id);
+          }.bind(this, data[i].id)}>
+           {data[i].title}
+        </a>
+        </li>);
       i = i +1;
     }
     return(
